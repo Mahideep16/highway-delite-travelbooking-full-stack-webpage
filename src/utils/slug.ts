21ce595404df4c098f4c56564c2978ccd71a484c
Mahideep16 <mahideepk75@gmail.com>
2025-11-01 00:15:38 +0530

    Delete src directory

--- src/utils/slug.ts
+++
@@ -1,8 +0,0 @@
-// Simple slugify helper to generate file-name friendly strings
-export function slugify(input: string): string {
-  return input
-    .toLowerCase()
-    .trim()
-    .replace(/[^a-z0-9]+/g, '-')
-    .replace(/(^-|-$)+/g, '');
-}
